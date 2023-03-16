4902f60c0ab7e465e80e22a93b3ef455de6217e1
dannyUribe <danny.uribeh@gmail.com>
2023-03-16 17:08:34 -0500

    Modifica CriptoDetalle boton

--- src/containers/Cripto/CriptoDetalle.jsx
+++ src/containers/Cripto/CriptoDetalle.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import CriptoDetalleTarjeta from '../../components/Tarjetas/CriptoDetalleTarjeta';
-import HttpService from '../../services/HttpService';
-
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import CriptoDetalleTarjeta from "../../components/Tarjetas/CriptoDetalleTarjeta";
+import HttpService from "../../services/HttpService";
 
 function CriptoDetalle() {
   const [crypto, setCrypto] = useState({});
@@ -13,20 +12,24 @@ function CriptoDetalle() {
 
   const consultarCripto = () => {
     console.log();
-    HttpService.Get('api/ticker/', {id:id})
+    HttpService.Get("api/ticker/", { id: id })
       .then((result) => {
         setCrypto(result.data[0]);
       })
       .catch((error) => {
         console.log("error: " + error);
       });
-  }
-  
+  };
+
   return (
     <div>
       <h1>Cripto Moneda </h1>
-      <Link to={'/cripto'}>Regresar</Link>
-      <CriptoDetalleTarjeta cryptoOut={crypto}/>
+      <div className="contButtonCD">
+        <button className="buttonCriptoDetalle">
+          <Link to={"/cripto"}>Regresar</Link>
+        </button>
+      </div>
+      <CriptoDetalleTarjeta cryptoOut={crypto} />
     </div>
   );
 }
